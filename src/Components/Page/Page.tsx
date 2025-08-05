a66ad8dd4f692af9ce74a3e89590348890d9ae57
Vladimir Noutbook <vovirus2@inbox.ru>
2025-08-05 17:10:10 +0300

    install material ui add Admin panel

--- src/Components/Page/Page.tsx
+++ src/Components/Page/Page.tsx
@@ -7,29 +7,27 @@ interface Props {
 export const Page: FC<Props> = ({ children }) => {
   return (
     <>
-      <>
-        <header className="header">
-          <div className="container">
-            <Navigation className="header-nav" placement="header" />
-          </div>
-        </header>
-        <main className="main">{children}</main>
-        <footer className="footer">
-          <div className="container">
-            <Navigation className="footer__nav" placement="footer" />
+      <header className="header">
+        <div className="container">
+          <Navigation className="header-nav" placement="header" />
+        </div>
+      </header>
+      <main className="main">{children}</main>
+      <footer className="footer">
+        <div className="container">
+          <Navigation className="footer__nav" placement="footer" />
 
-            <div className="footer__column">
-              <p className="footer__text">
-                Все вопросы
-                <a href="https://vk.com/vovirus" className="footer__link" target="_blank">
-                  Владимир
-                </a>
-              </p>
-              <p className="footer__copy">@ 2024</p>
-            </div>
+          <div className="footer__column">
+            <p className="footer__text">
+              Все вопросы
+              <a href="https://vk.com/vovirus" className="footer__link" target="_blank">
+                Владимир
+              </a>
+            </p>
+            <p className="footer__copy">@ 2024</p>
           </div>
-        </footer>
-      </>
+        </div>
+      </footer>
     </>
   );
 };

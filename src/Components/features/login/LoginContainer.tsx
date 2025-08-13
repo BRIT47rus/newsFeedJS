04417b03b1d78902eb75ee157a47550cf1b9fd0a
Vladimir Noutbook <vovirus2@inbox.ru>
2025-08-13 13:22:20 +0300

    add to validate email

--- src/Components/features/login/LoginContainer.tsx
+++ src/Components/features/login/LoginContainer.tsx
@@ -1,21 +1,27 @@
 import React, { useReducer } from 'react';
 import './LoginContainer.css';
 import { LoginForm, TLoginField } from '../../LoginForm/LoginForm';
+import { validateEmail } from './utils';
 type TLoginFieldState = Omit<TLoginField, 'onChange'>;
 type TAction = {
-  type: 'change';
+  type: 'change' | 'error';
   value: string;
 };
 const reducer = (state: TLoginFieldState, action: TAction): TLoginFieldState => {
   switch (action.type) {
-    case 'change':
-      return {
-        ...state,
-        value: action.value,
-      };
+  case 'change':
+    return {
+      ...state,
+      value: action.value,
+    };
+  case 'error':
+    return {
+      ...state,
+      value: action.value,
+    };
 
-    default:
-      break;
+  default:
+    break;
   }
   return state;
 };
@@ -30,6 +36,17 @@ export const LoginContainer = () => {
     value: '',
   });
 
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    let isValid = true;
+    if (!validateEmail(emailState.value)) {
+      isValid = false;
+      dispatchEmail({ type: 'error', value: 'Введите корректный email' });
+    } else if (isValid) {
+      isValid = true;
+    }
+  };
+
   return (
     <div className="login-container">
       <LoginForm
@@ -38,7 +55,7 @@ export const LoginContainer = () => {
           ...passwordState,
           onChange: (e) => dispatchPasswordState({ type: 'change', value: e.currentTarget.value }),
         }}
-        onSubmit={console.log}
+        onSubmit={onSubmit}
       />
     </div>
   );
